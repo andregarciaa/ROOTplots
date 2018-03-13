16deb2c9a84239458d74c23fa5418a1200da96e2
andregarciaa <garciaa@ifca.unican.es>
2018-03-13 12:51:16 +0100

    Add script info header. Correct substring splitings

--- root_plots.py
+++ root_plots.py
@@ -1,38 +1,48 @@
 #!/usr/bin/env python
 # writen in Python, so look for command interpreter in this path
 
-# Script which creates ROOT plots from a ROOT file. Modified for particular case of 
-# charge distribution of 3D sensors from May 2017 SPS TB.
+import os, glob
 
-from os import listdir
+"""
+Script which creates ROOT plots from a ROOT file. Modified for particular case of
+charge distribution of 3D sensors from May 2017 SPS TB.
+
+__author__ = "Andrea Garcia Alonso"
+__copyright__ = "Copyright 2018"
+__version__ = "1.0"
+__email__ = "andrea.garcia.alonso@cern.ch"
+
+"""
 
 #----------------------------------------------------------------------------
 
-# First method (quite simple) from path, check there is a root file in the directory and 
-# saves two strings with the type of the sensor and the run number (useful for plot titles or legends):
+# 1st method (quite simple) from path, check there is a root file in the directory and 
+# saves 2 strings with the sensor type and the run number (useful for plot titles, legends):
 
 read_path():
 
     # list all the "beam_analysis_cluster.root" files inside any folder of the given path:
     files = glob.glob('/eos/user/d/duarte/alibavas_data_root/**/*beam_analysis_cluster.root', recursive=True)
   
-    # if list is empty... raise error:
+    # if list is empty, raise error:
     if(files == []): raise IOError("\033[1;35mThe given directory does not contain the necessary ROOT file/s\033[1;m")
 
     read path_with_root_file
     # example of path_with_root_file: 
     # /eos/user/d/duarte/alibavas_data_root/N1-7_7e15_b2/run000391/
-    #           391_2017-05-21_15-26_gerva_MBV3_N1-7_-200V_-31d2uA_-25C_lat132_beam_analysis_cluster.root
+    #   391_2017-05-21_15-26_gerva_MBV3_N1-7_-200V_-31d2uA_-25C_lat132_beam_analysis_cluster.root
 
-    sensor_type = substring of path_with_root_file after "data_root/"
+    # substring of path_with_root_file containing the sensor type (spliting with "/": 7th place)
+    sensor_type = path_with_root_file.split("/")[6]
 
-    run_number = substring of path_with_root_file after "/run000"
+    # substring of path_with_root_file after sensor type, removing "run000" from the begining:
+    run_number = path_with_root_file.split("/")[7].replace("run000","")
 
     return sensor_type, run_number
 
 #----------------------------------------------------------------------------
 
-# 2nd method look for the branch name given by the user, checking the tree "alibava_clusters" inside the ROOT file:
+# 2nd method look for the branch name given by user, checking "alibava_clusters" tree inside ROOT file:
 
 plot_branch(path_with_root_file, branch_to_plot, sensor_type, run_number):
 
