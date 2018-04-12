0df51bf0f11bd5f60c547f6b544252a31b8c8edb
andregarciaa <garciaa@ifca.unican.es>
2018-04-12 13:28:16 +0200

    If there's no eventTime branch, ignore that file.

--- root_plots.py
+++ root_plots.py
@@ -101,6 +101,7 @@ def process(sensor_run_path_dic):
             if(hasattr(root_tree, "eventTime")=="False"): 
                 raise RunTimeError("\033[1;mThere is no eventTime branch in the tree of \
                 sensor {0} at run {1}.format(sensor, run)\033[1;m")
+                continue
                 
             # Obtain the time window and save in a list of 2 elements:
             time_window = an.get_time_window(root_tree,"eventTime")
