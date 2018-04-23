0ab0cc30560cf0b927f881abe0ebaa488ef9bc25
andregarciaa <garciaa@ifca.unican.es>
2018-04-23 10:36:16 +0200

    Correct logbooks.

--- root_plots.py
+++ root_plots.py
@@ -12,8 +12,12 @@ sifca_style=set_sifca_style(stat_off=False)
 sifca_style.cd()  # esto quizas no es necesario, pero no hace danyo
 
 """
-Script which creates ROOT plots from a ROOT file. Modified for particular case of
-charge distribution of 3D sensors from May 2017 SPS TB.
+Script which creates a PDF file with the canvas of the calibrated charge 
+distributions obtained from a path with several ROOT files. 
+Two particular cases of charge distribution of 3D sensors can be selected 
+through hardcoding: the six 3D sensors measured at May 2017 SPS Test Beam 
+or only M1-5 and N1-3 from TB and RS of 2016 and 2017 (in order to compare 
+them to study excessive collected charge by M1-5).
 
 __author__ = "Andrea Garcia Alonso"
 __copyright__ = "Copyright 2018"
@@ -30,8 +34,8 @@ def read_path():
     Method read_path() checks the path where the root files are stored and saves them all
     (including their full paths) in a list. Then, it goes down this list saving in a
     dictionary the sensors names related with all the runs of each one and the full path.
-    Input: nothing
-    Returns: dictionary relating sensor_name--run_number--full_path
+    - Input: nothing
+    - Output: sensor_run_path_dic: dictionary relating sensor_name--run_number--full_path
     """
 
     # FOR ALL THE 3D SENSORS OF TB 2017:---------------------------------------------------------------
@@ -109,11 +113,11 @@ def process(sensor_run_path_dic):
     the ROOT files which have been stored in sensor_run_path_dic dictionary.
     Creates a pdf with all the canvas, doing a Landau-Gauss
     fit and computing the fit variables. MPVs are stored in a dictionary:
-    input: sensor_run_path_dic dictionary.
-    output: pdf with all the calibrated charge distribution plots and a dictionary
-    with all the MPV of the Landau-Gauss fits for those distributions.
-
+    - Input: sensor_run_path_dic: dictionary
+    - Outputs: pdf file. All the fitted calibrated charge distribution plots
+               mpv_values: dictionary. MPVs of the Landau-Gauss fits
     """
+
     ROOT.gROOT.SetBatch()
     mpv_values = {}
 
